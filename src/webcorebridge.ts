1b837429d68e7892da0c493c5cbee55a97bca25c
Victor Timoshin <bonus85@VIKI-752.localhost>
2019-11-19 10:48:58 +0300

    Initial commit

--- src/webcorebridge.ts
+++ src/webcorebridge.ts
@@ -1,23 +1,24 @@
 "use strict";
 
 import * as _ from "underscore";
-import * as Module from "../../sway.module_webapp-mvc/bin/sway.module_webcore.js";
 import { IWebcoreBridgeModule } from "./webcorebridgemodule";
 
 export class WebcoreBridge {
+	public module: IWebcoreBridgeModule = null;
+
 	private static instance: WebcoreBridge;
-	static getInstance(): WebcoreBridge {
-		WebcoreBridge.instance = WebcoreBridge.instance || new WebcoreBridge();
+	static getInstance(moduleObj: any): WebcoreBridge {
+		WebcoreBridge.instance = WebcoreBridge.instance || new WebcoreBridge(moduleObj);
 		return WebcoreBridge.instance;
 	}
 
 	private _isInitialized: boolean = false;
 
-	constructor(public module: IWebcoreBridgeModule = null) {
+	constructor(moduleObj: any) {
 		if (WebcoreBridge.instance)
 			throw new Error("Error - use Singleton.getInstance()");
 
-		this.module = Module();
+		this.module = moduleObj;
 	}
 
 	public initialize(timeout: number = 3000): Promise<boolean> {
