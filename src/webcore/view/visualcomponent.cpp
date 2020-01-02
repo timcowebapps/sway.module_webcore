5ba36483d728afc10bb6856563ac727cafbd880c
Victor Timoshin <bonus85@VIKI-752.localhost>
2020-01-02 03:22:21 +0300

    Рефакторинг

---
+++ src/webcore/view/visualcomponent.cpp
@@ -0,0 +1,49 @@
+#include <sway/webcore/view/visualcomponent.h>
+#include <sway/webcore/base/treeupdater.h>
+
+NAMESPACE_BEGIN(sway)
+NAMESPACE_BEGIN(webcore)
+NAMESPACE_BEGIN(view)
+
+void AVisualComponent::registerEmscriptenClass(lpcstr_t classname) {
+	emscripten::class_<AVisualComponent, emscripten::base<base::TreeNodeElement>>(classname)
+		.constructor<core::containers::HierarchyNodePtr_t,
+			core::containers::HierarchyNodeIndex, std::string, base::TreeNodeElementCreateInfo>()
+		.function("getModel", &AVisualComponent::getModel, emscripten::allow_raw_pointers())
+		.function("setModel", &AVisualComponent::setModel, emscripten::allow_raw_pointers());
+}
+
+AVisualComponent::AVisualComponent(core::containers::HierarchyNodePtr_t parent,
+	const core::containers::HierarchyNodeIndex & nodeIndex,
+	const std::string & nodeId, const base::TreeNodeElementCreateInfo & createInfo)
+	: base::TreeNodeElement(parent, nodeIndex, nodeId, createInfo) {
+	// Empty
+}
+
+void AVisualComponent::accept(base::ITreeVisitor * visitor) {
+	visitor->visitOnEnter(this);
+
+	for (core::containers::HierarchyNodePtr_t node : getChildren())
+		static_cast<AVisualComponent *>(node)->accept(visitor);
+}
+
+void AVisualComponent::initialize() {
+	// Empty
+}
+
+void AVisualComponent::update() {
+	// Empty
+}
+
+core::utilities::Observable * AVisualComponent::getModel() {
+	return _model;
+}
+
+void AVisualComponent::setModel(core::utilities::Observable * model) {
+	_model = model;
+	_model->registerObserver(this);
+}
+
+NAMESPACE_END(base)
+NAMESPACE_END(webcore)
+NAMESPACE_END(sway)
